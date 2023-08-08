5961d500f690473d6b1ebea887d9bf83d0b6ea30
root <kelvinthuo999>
2023-08-08 19:30:11 +0000

    print last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,19 +13,13 @@ else:
 
 if last_digit > 5:
     # Check if the last digit is greater than 5
-    print(f"Last digit of {number}, followed by")
-    print(f"the number, followed by")
-    print(f"the string is {last_digit} and is greater than 5")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
     # Print the appropriate message if the last digit is greater than 5
 elif last_digit == 0:
     # Check if the last digit is 0
-    print(f"Last digit of {number}, followed by")
-    print(f"the number, followed by")
-    print(f"the string is 0 and is 0")
+    print(f"Last digit of {number} is 0 and is 0")
     # Print the appropriate message if the last digit is 0
 else:
     # If the last digit is not greater than 5 or 0, it must be between 1 and 5
-    print(f"Last digit of {number}, followed by")
-    print(f"the number, followed by")
-    print(f"the string is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
     # Print the appropriate message if the last digit is between 1 and 5
