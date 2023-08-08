bd6450bfc3c60b04e36d5e3092bae7a024df4aec
root <kelvinthuo999>
2023-08-08 19:38:23 +0000

    added new comments

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
+    # Iterate through ASCII values for lowercase letters (97 to 122)
     print("{:c}".format(i), end='')
+    # Print the character corresponding to the ASCII value
