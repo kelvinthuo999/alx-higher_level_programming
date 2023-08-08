299a0ffe966a3368178d7d0ebe62e3cfe01e65b9
root <kelvinthuo999>
2023-08-08 19:11:48 +0000

    print the last digit correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,30 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
+    # Check if 'number' is negative
     last_digit = number % -10
+    # If negative, find the remainder of the division of 'number' by -10
+    # This will give us the last digit as a negative number
 else:
     last_digit = number % 10
+    # If positive or zero, find the remainder of the division of 'number' by 10
+    # This will give us the last digit
 
 if last_digit > 5:
-    print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
-elif 0 < last_digit < 6:
-    print(f"Last digit of {number:d} is {last_digit:d} and is less than 6 and not 0")
+    # Check if the last digit is greater than 5
+    print(f"Last digit of {number}, followed by")
+    print(f"the number, followed by")
+    print(f"the string is {last_digit} and is greater than 5")
+    # Print the appropriate message if the last digit is greater than 5
+elif last_digit == 0:
+    # Check if the last digit is 0
+    print(f"Last digit of {number}, followed by")
+    print(f"the number, followed by")
+    print(f"the string is 0 and is 0")
+    # Print the appropriate message if the last digit is 0
 else:
-    print(f"Last digit of {number:d} is 0 and is 0")
+    # If the last digit is not greater than 5 or 0, it must be between 1 and 5
+    print(f"Last digit of {number}, followed by")
+    print(f"the number, followed by")
+    print(f"the string is {last_digit} and is less than 6 and not 0")
+    # Print the appropriate message if the last digit is between 1 and 5
