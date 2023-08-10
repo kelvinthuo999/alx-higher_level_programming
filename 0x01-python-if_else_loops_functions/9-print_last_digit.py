c2c9c32813f45ebeb319fb68a1c4faa5342976ad
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-10 04:03:13 -0700

    removed whitespaces

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,12 +5,12 @@ Print the Last Digit of a Number
 This function calculates and prints the last digit of a given number.
 """
 
+
 def print_last_digit(number):
     # Calculate the last digit
-    last_digit = abs(number) % 10
-    
+    last_digit = abs(number) % 1
+
     # Print the last digit without a newline
     print("{:d}".format(last_digit), end='')
-    
     # Return the last digit
     return last_digit
