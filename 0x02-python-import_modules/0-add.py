b93a8d8e4b5610b6570c8292732907f79bf9bcc6
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-11 00:38:26 -0700

    removed whitespaces

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Check if the script is being run as the main program """
+# Check if the script is being run as the main program
 if __name__ == "__main__":
     from add_0 import add
 
