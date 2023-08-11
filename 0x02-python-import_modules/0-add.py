49c45fd7b43fe71930d969d69303688c126d4759
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-11 00:39:25 -0700

    removed whitespaces

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Check if the script is being run as the main program
 if __name__ == "__main__":
+    # Import the 'add' function from the 'add_0' module
     from add_0 import add
 
     a = 1
