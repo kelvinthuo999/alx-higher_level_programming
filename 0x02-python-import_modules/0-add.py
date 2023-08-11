05c58e733b9d1957e1d057bbafa7a574b451da12
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-11 00:26:14 -0700

    add two variables using imported function

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/bin/bash/python3
+
+# Check if the script is being run as the main program
+if __name__ == "__main__":
+    # Import the 'add' function from the 'add_0' module
+    from add_0 import add
+
+    # Assign values to respective variables
+    a = 1
+    b = 2
+
+    # Use string to print the values and result for the addition
+    print('{:d} + {:d} = {:d}' .format(a, b, add(a, b)))
