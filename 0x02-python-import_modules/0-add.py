440f953ffb332d757643b707faaeb02d7c94738e
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-11 00:40:29 -0700

    removed whitespaces

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # Check if the script is being run as the main program
 if __name__ == "__main__":
-    # Import the 'add' function from the 'add_0' module
     from add_0 import add
 
+    # Assign values to variables for addition
     a = 1
     b = 2
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
