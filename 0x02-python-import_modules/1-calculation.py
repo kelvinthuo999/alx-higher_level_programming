3198da7cfa37d3f8ab52eab71eba9838de5ac0ea
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-11 01:45:54 -0700

    meets pycodestyle

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-""" Check if the script is being run as the main program """
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-
     # set values to specific variables
     a = 10
     b = 5
-
     # print values and perform respective arithmetic operation
     print('{:d} + {:d} = {:d}'.format(a, b, (add(a, b))))
     print('{:d} - {:d} = {:d}'.format(a, b, (sub(a, b))))
