44cf4ee0a4082c2158582ceb30cc336273132ba3
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-11 00:34:40 -0700

    removed whitespaces

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,10 +4,9 @@
 if __name__ == "__main__":
     # Import the 'add' function from the 'add_0' module
     from add_0 import add
-
     # Assign values to respective variables
     a = 1
     b = 2
 
     # Use string to print the values and result for the addition
-    print('{:d} + {:d} = {:d}' .format(a, b, add(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
