77185d73add54568a53ec3300f6d8ac7edb1373b
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-14 04:23:51 -0700

    create a copy of a list and change an element

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" replace an element without modifying the original list """
+
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+    else:
+        list_copy = my_list.copy()
+        list_copy[idx] = element
+        return (list_copy)
