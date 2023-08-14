835e62fee91498341ebf61995367dcd2141227a3
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-14 08:30:55 -0700

    evaluate the largest integer in the list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" evaluate the largest integer """
+
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        largest = my_list[0] 
+        for i in range(len(my_list)):
+            if my_list[i] > largest:
+                largest = my_list[i]
+        return (largest)
