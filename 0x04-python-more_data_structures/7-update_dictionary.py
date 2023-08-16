0bcace61c703d5d8f615c18232dee9077fad3397
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-16 07:45:39 -0700

    function to update or replace key/values pairs

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" function that adds or replaces key/value pairs """
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+
+    return (a_dictionary)
