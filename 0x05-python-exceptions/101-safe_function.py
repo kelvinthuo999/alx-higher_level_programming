5410e00afdd88a9da0a97b2d2891fc2c2af0681c
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-28 06:10:09 -0700

    reintroduced a variable

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,8 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        return (fct(*args))
+        ans = fct(*args)
+        return (ans)
     except Exception as e:
-        print("Exception: ".format(e), file=sys.stderr)
+        print("Exception: {} ".format(e), file=sys.stderr)
         return (None)
