0cabf3b49a5d7a293860d2ff8a10cdfe03a37791
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-28 03:43:53 -0700

    function to print integers in a list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" function to print x integer elements in a list """
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    printed_elements = 0
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                printed_elements += 1
+            except (ValueError, TypeError):
+                pass
+    except (IndexError):
+                pass
+    finally:
+        print()
+    return (printed_elements)
