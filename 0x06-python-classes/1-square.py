cd8e6236253d70b6ccd828cdeb607bc836814594
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-29 05:27:45 -0700

    changed the comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
+""" definition of a class """
 
 class Square:
-    """
-    This class defines a square.
-
-    Attributes:
-        size (int): The size of the square.
+    """ this class defines a square.
+    Attributes: size (int): The size of the square.
     """
 
     def __init__(self, size):
-        """
-        Initializes a new instance of the Square class.
-
-        Args:
-            size (int): The size of the square.
+        """Initializes a new instance of the Square class.
+        Args:size (int): The size of the square.
         """
         self.__size = size
