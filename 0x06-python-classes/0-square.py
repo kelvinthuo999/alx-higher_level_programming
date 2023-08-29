85d9c3c6eef741199c7cc851ed660526d8ccc8f8
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-29 05:11:29 -0700

    fourth trial on empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""And empty square class."""
-
 
 class Square:
-    """And empty square class with an artribute"""
+    """This is the Square class.It defines a square."""
     pass
