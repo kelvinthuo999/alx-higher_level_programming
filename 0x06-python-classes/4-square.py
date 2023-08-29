344c04f455c6c82447249f44776b345a9c0adf73
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-08-29 05:49:05 -0700

    a class implementing a setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Definition of a class """
+
+
+class Square:
+    """
+    This class defines a square.
+    Attributes:__size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+        Args:size (int, optional): The size defaults to 0.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Getter method for retrieving the size of the square.
+        Returns:int: The size of the square.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method for setting the size of the square.
+        Args:value (int): The new size for the square.
+        Raises:TypeError: If value is not an integer.
+               ValueError: If value is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates the area of the square.
+        Returns:int: The area of the square.
+        """
+        return (self.__size ** 2)
