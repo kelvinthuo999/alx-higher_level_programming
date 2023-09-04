57df3b393a2054c92e89691c648b50422c73f4d8
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-09-04 03:19:01 -0700

    string representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""" a class that defines a rectangle """
+
+
+class Rectangle:
+    """A class that defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a rectangle with optional width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate and return the rectangle area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculate and return the rectangle perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect_str = ""
+        for i in range(self.__height):
+            rect_str += "#" * self.__width + "\n"
+        return rect_str[:-1]  # Remove the trailing newline
+
+    def __repr__(self):
+        """Return a string rep of the rectangle for debugging."""
+        return f"Rectangle({self.__width}, {self.__height})"
