63296263c0f933e8593e1dff94e1a3c1cdab2f9c
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-10-18 05:41:57 -0700

    filter shows depending on specifications

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- List shows without a genre linked
+SELECT tv_shows.title, 'NULL' AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, genre_id;
