af4e35d2f4c3645cd40221421e26d17a59f17d3a
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-11-14 23:13:28 -0800

    script to concatenate the first two arguments

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the first and second arguments passed to the script
+const firstArgument = process.argv[2];
+const secondArgument = process.argv[3];
+
+// Use console.log(...) to print the output in the specified format
+console.log(`${firstArgument} is ${secondArgument}`);
