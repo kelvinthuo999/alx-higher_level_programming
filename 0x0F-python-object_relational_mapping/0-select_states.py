1a67fe917d6e7d692f07b051a417736d9ce9bf76
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-11-18 04:04:09 -0800

    second trial: script to select all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the
+database specified by command-line arguments.
+"""
+
 import MySQLdb
-import sys
+from sys import argv
 
 def list_states(username, password, db_name):
+    """
+    Connect to the MySQL server and list all states
+    from the specified database.
+    """
     try:
         # Connect to the MySQL server
-        connection = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+        connection = MySQLdb.connect(
+            host="localhost", port=3306, user=username, passwd=password, db=db_name)
 
         # Create a cursor object to interact with the database
         cursor = connection.cursor()
 
-        # Execute the SQL query to fetch all states sorted by states.id
-        query = "SELECT * FROM states ORDER BY states.id"
+        # Execute the SQL query to fetch all states
+        query = "SELECT * FROM states"
         cursor.execute(query)
 
         # Fetch all the results
@@ -32,15 +42,17 @@ def list_states(username, password, db_name):
             connection.close()
 
 if __name__ == "__main__":
+    """
+    Access the database and get the states
+    from the database using command-line arguments.
+    """
     # Check if all three arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <db_name>".format(sys.argv[0]))
-        sys.exit(1)
+    if len(argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <db_name>".format(argv[0]))
+        exit(1)
 
     # Get MySQL username, password, and database name from command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
+    mysql_username, mysql_password, db_name = argv[1], argv[2], argv[3]
 
     # Call the function to list states
     list_states(mysql_username, mysql_password, db_name)
