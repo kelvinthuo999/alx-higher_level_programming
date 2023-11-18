fdcdc6bddcb7924754fcfaab3a1145f828790cc1
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-11-18 05:11:44 -0800

    modified approach style

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """
-This script defines the State class and creates an instance of Base using SQLAlchemy.
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
 """
-
+import sys
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-# Replace 'mysql_user', 'mysql_password', 'database_name' with your MySQL credentials
-DATABASE_URI = 'mysql+mysqldb://mysql_user:mysql_password@localhost:3306/database_name'
 
 Base = declarative_base()
 
@@ -21,15 +18,7 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
-# Connect to the MySQL server
-engine = create_engine(DATABASE_URI)
-
-# Create all tables in the database
-Base.metadata.create_all(engine)
-
-# Create a session to interact with the database
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Close the session
-session.close()
+if __name__ == "__main__":
+    # Replace 'mysql_user', 'mysql_password', 'database_name' with your MySQL credentials
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
