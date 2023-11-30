339faa53446abea5b94b73c797a96a0350b12399
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-11-30 06:55:03 -0800

    create: script to send post request

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script sends a POST request to a URL with custom variables and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
