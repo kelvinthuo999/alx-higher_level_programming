3524b0a4f5b2b600354b1e8a38284f1e660d3e64
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-11-30 06:52:32 -0800

    create: script to send get request

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends an OPTIONS request to a URL and displays the allowed HTTP methods
-curl -s -i -X OPTIONS "$1" | grep -i allow | cut -d " " -f 2-
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
