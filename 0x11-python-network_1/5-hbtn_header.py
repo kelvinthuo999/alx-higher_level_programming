017feac55d7a4c6c738f201d2da7573fc54afd02
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-12-01 08:07:29 -0800

    create: script to display the value of a variable

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script to send a request to a URL and display the value of X-Request-Id.
+"""
+
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: ./5-hbtn_header.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+response = requests.get(url)
+
+x_request_id = response.headers.get('X-Request-Id')
+
+print(x_request_id)
